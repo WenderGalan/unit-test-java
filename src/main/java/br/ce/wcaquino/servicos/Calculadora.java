7c265195b3e347948811c44d5e60f25342d52285
Wender <wendergalan2014@hotmail.com>
2019-12-04 20:34:26 -0400

    Finalizando o primeiro modulo..

--- src/main/java/br/ce/wcaquino/servicos/Calculadora.java
+++ src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -17,4 +17,8 @@ public class Calculadora {
             throw new NaoPodeDividirPorZeroException();
         return a / b;
     }
+
+    public int divide(String a, String b) {
+        return Integer.parseInt(a) / Integer.parseInt(b);
+    }
 }
